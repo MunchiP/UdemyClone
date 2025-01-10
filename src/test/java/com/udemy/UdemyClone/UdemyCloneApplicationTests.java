b121bd0eb5042e38faa9f8d4f57651d7c19c4c3f
MrFackry <daniel.d.arcos05gmail.com>
2025-01-10 10:21:07 -0500

    se creo el proyecto backend sin esctructura

---
+++ src/test/java/com/udemy/UdemyClone/UdemyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.UdemyClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
