6df11c51687ba8049f9254fe04be8a04e1c9629f
MunchiP <paulaviveusa@gmail.com>
2025-01-13 09:03:11 -0500

    Se agrega Angular Cli y estructura del proyecto

---
+++ udemyClone/src/app/Componentes/Privado/editar-datos-usuario/editar-datos-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-datos-usuario',
+  imports: [],
+  templateUrl: './editar-datos-usuario.component.html',
+  styleUrl: './editar-datos-usuario.component.css'
+})
+export class EditarDatosUsuarioComponent {
+
+}
