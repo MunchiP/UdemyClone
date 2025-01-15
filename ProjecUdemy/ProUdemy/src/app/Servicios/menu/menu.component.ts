c46d23f027f9ef41507caf1ada7954fa55ebdc12
MunchiP <paulaviveusa@gmail.com>
2025-01-15 14:29:33 -0500

    Estructura y avance del menú

--- ProjecUdemy/ProUdemy/src/app/Servicios/menu/menu.component.ts
+++ ProjecUdemy/ProUdemy/src/app/Servicios/menu/menu.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+// Importación de elementos de Angular Material
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
