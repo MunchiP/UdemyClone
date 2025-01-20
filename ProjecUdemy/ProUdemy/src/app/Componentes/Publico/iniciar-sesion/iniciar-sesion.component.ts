c8df535d6878b9eb57c1d5301c843457d7c89cf6
MunchiP <paulaviveusa@gmail.com>
2025-01-20 12:29:44 -0500

    Proyecto con codigo de connection a back

--- ProjecUdemy/ProUdemy/src/app/Componentes/Publico/iniciar-sesion/iniciar-sesion.component.ts
+++ ProjecUdemy/ProUdemy/src/app/Componentes/Publico/iniciar-sesion/iniciar-sesion.component.ts
@@ -4,17 +4,60 @@ import { Component } from '@angular/core';
 // import { Router } from '@angular/router';
 import { MenuComponent } from '../../../Servicios/menu/menu.component';
 import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+// Los mínimos para que un formulario funcione
+import { FormGroup, FormBuilder, Validators, } from '@angular/forms';
+// El único que se exporta abajo
+import {  ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-iniciar-sesion',
   imports: [
     // Router,
-    MenuComponent,
-    MatIconModule
+    // MenuComponent,
+    ReactiveFormsModule,
+    MatIconModule,
+    CommonModule,
+    HttpClientModule
   ],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
 export class IniciarSesionComponent {
+ formularioInicioSesion: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+  ) {
+    this.formularioInicioSesion = this.fb.group({
+      correo:['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    })
+  }
+
+  onSubmit(): void{
+    if(this.formularioInicioSesion){
+      const credenciales = this.formularioInicioSesion.value;
+
+      this.http.post('arcos', credenciales).subscribe({
+        next:(response: any) => {
+          console.log('inicio exitoso', response);
+          // localStorage.setItem('token',response.token)
+          this.router.navigate(['/InicioPrivado'])
+        },
+        error: (error) => {
+          console.error('Error en el inicio de sesion', error);
+          alert('Correo o contraseña incorrectos')
+        },
+      })
+    } else {
+      alert('Por favor completa el formulario correctamente')
+    }
+  }
 
 }
--- ProjecUdemy/ProUdemy/src/app/Componentes/Publico/registrarse/registrarse.component.ts
+++ ProjecUdemy/ProUdemy/src/app/Componentes/Publico/registrarse/registrarse.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import { MenuComponent } from '../../../Servicios/menu/menu.component';
-
+import { HttpClient } from '@angular/common/http';
 
 
 // funcionamiento de igif
@@ -15,17 +15,19 @@ import { CommonModule } from '@angular/common';
   imports: [
     // Importación de la linea arriba
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
     // MenuComponent
 
   ],
   templateUrl: './registrarse.component.html',
   styleUrl: './registrarse.component.css'
 })
+
+
 export class RegistrarseComponent {
   formularioRegistro: FormGroup; // Define la propiedad formularioRegistro
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.formularioRegistro = this.fb.group({
       nombre: ['', Validators.required],
       correo: ['', [Validators.required, Validators.email]],
@@ -33,11 +35,22 @@ export class RegistrarseComponent {
     });
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if (this.formularioRegistro.valid) {
-      console.log('Formulario válido:', this.formularioRegistro.value);
-    } else {
-      console.log('Formulario inválido');
-    }
+      const datosFormulario = this.formularioRegistro.value;
+      this.http.post('http://localhost', datosFormulario).subscribe({
+        next: (response) => {
+          console.log("Usuario registado:", response);
+          alert("Usuario registrado exitosamente");
+        },
+
+        error: (error) => {
+          console.error("Error al registrar usuario:", error);
+          alert("Error al registrar usuario");
+        },
+      });
+     } else {
+      alert("Por favor completa el formulario correctamente")
   }
+}
 }
