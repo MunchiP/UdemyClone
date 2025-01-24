003437e33befef0080b9271635e408f999992978
MunchiP <paulaviveusa@gmail.com>
2025-01-24 17:54:43 -0500

    modificacion de correo a email

--- ProjecUdemy/ProUdemy/dist/pro-udemy/server/angular-app-manifest.mjs
+++ ProjecUdemy/ProUdemy/dist/pro-udemy/server/angular-app-manifest.mjs
@@ -23,11 +23,11 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 24309, hash: '996bb8e13c1a417089ea120a94a2141a8157be6579dea3121da3ccf6c5d68ea6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17883, hash: 'd52543d8818aab9391e250000c8ffd9dcf046ac7cd42bc1e79a474d1ed2303aa', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'iniciar-sesion/index.html': {size: 46374, hash: '1000f3b3b574eae799bc52474a42e2dcc8b7cef7f86ea01c5c535cc7613d63e7', text: () => import('./assets-chunks/iniciar-sesion_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 44255, hash: '53b5a2f7934be38c727f2e899c29cfdf45a6092f59275610196f4c93f4550cbe', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'registrarse/index.html': {size: 44307, hash: 'fab655622e9f6d7c941208d39ebb6cc21426c59abe5a6fdaa1e1de8e15832384', text: () => import('./assets-chunks/registrarse_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 24309, hash: '23a74a27c2fee8004684ee5d0813d409d8b2a65b2bdc0c723c493b367a83d77f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17883, hash: '84ea77c56f9fd6907b6c5733810afb9baeeec77cf685a60737814b4f28b3676c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'iniciar-sesion/index.html': {size: 48266, hash: 'c7fcdb93268eebf31bd454386a9b1267649fccf4aec4282b6075f051eee6c17e', text: () => import('./assets-chunks/iniciar-sesion_index_html.mjs').then(m => m.default)},
+    'registrarse/index.html': {size: 47434, hash: 'bb63264de94105ef883db858836aff89cd34148e11f5b7d8dc185aecd198fafe', text: () => import('./assets-chunks/registrarse_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 44334, hash: '3deac5f7062c3778c70b63c839511fe48cb1187a77482a4678c774bd717ac081', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-CXQUZ3PB.css': {size: 6979, hash: 'mYIPdabeAag', text: () => import('./assets-chunks/styles-CXQUZ3PB_css.mjs').then(m => m.default)}
   },
 };
