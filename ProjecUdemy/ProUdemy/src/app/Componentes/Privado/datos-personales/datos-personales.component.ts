e0c7bb4d4d032fe50e2534c41da797b4694326e6
MunchiP <paulaviveusa@gmail.com>
2025-01-24 20:37:38 -0500

    se agregan metodo de nombre y apellido para backend

--- ProjecUdemy/ProUdemy/src/app/Componentes/Privado/datos-personales/datos-personales.component.ts
+++ ProjecUdemy/ProUdemy/src/app/Componentes/Privado/datos-personales/datos-personales.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// Componente importado OJOOOOOOOOO Json
+import { SolicitudesComponent, NombreResponse, ApellidoResponse } from '../../../Servicios/solicitudes/solicitudes.component';
+
+// Componente importado OJOOOOOOOOO String
+// import { SolicitudesComponent, } from '../../../Servicios/solicitudes/solicitudes.component';
+
+
 
 @Component({
   selector: 'app-datos-personales',
-  imports: [],
+  imports: [
+    // SolicitudesComponent,
+  ],
   templateUrl: './datos-personales.component.html',
   styleUrl: './datos-personales.component.css'
 })
-export class DatosPersonalesComponent {
 
+export class DatosPersonalesComponent implements OnInit {
+  nombre: string = '';
+  apellido: string = '';
+
+  constructor(private solicitudesComponent: SolicitudesComponent) {}
+
+  ngOnInit(): void {
+    this.solicitudesComponent.getNombre().subscribe({
+      next: (response) => {
+        // Guardo el nombre que recibo en la variable
+        // this.nombre = response;
+        // como Arcos me envía un objeto y no sólo la variable específica debería usarlo así:
+        this.nombre = response.nombre;
+      }, error: (err) =>{
+        console.error('Error en el nombre', err);
+      }
+    }), 
+    this.solicitudesComponent.getApellido().subscribe({
+      next: (response) => {
+        // Guardo el nombre que recibo en la variable
+        this.apellido = response.apellido;
+      }, error: (err) =>{
+        console.error('Error en el apellido', err);
+      }
+    })
+  }
 }
---
+++ ProjecUdemy/ProUdemy/src/app/Servicios/solicitudes/solicitudes.component.ts
@@ -0,0 +1,57 @@
+// En caso de que el Backend me responda un string específico, sería así:
+
+// import { Component } from '@angular/core';
+// import { HttpClientModule } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Component({
+//   selector: 'app-solicitudes',
+//   imports: [
+//     HttpClientModule,
+//   ],
+//   templateUrl: './solicitudes.component.html',
+//   styleUrl: './solicitudes.component.css'
+// })
+
+// export class SolicitudesComponent {
+//   private apiUrl = 'http://localhost:8080/api/udemy/user/data';
+
+//   constructor(private http: HttpClient){}
+
+//   getNombre(): Observable<string>{
+//     return this.http.get<string>(`${this.apiUrl}/nombre`);
+//   }
+
+//   getApellido(): Observable<string>{
+//     return this.http.get<string>(`${this.apiUrl}/apellido`);
+//   }
+// }
+
+// En caso de que el Backend me responda un objeto JSON, sería así:
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface NombreResponse {
+  nombre: string;
+}
+
+export interface ApellidoResponse {
+  apellido: string;
+}
+
+export class SolicitudesComponent {
+  private apiUrl = 'http://localhost:8080/api/udemy/user/data';
+
+  constructor(private http: HttpClient) {}
+
+  getNombre(): Observable<NombreResponse> {
+    return this.http.get<NombreResponse>(`${this.apiUrl}/nombre`);
+  }
+
+  getApellido(): Observable<ApellidoResponse> {
+    return this.http.get<ApellidoResponse>(`${this.apiUrl}/apellido`);
+  }
+}
